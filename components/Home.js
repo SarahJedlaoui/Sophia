e2f1d4acbe94890481b4fc5bebf27a4cf57c3853
SarahJedlaoui <sarajedlaoui999@gmail.com>
2025-02-11 15:11:49 +0100

    updates

--- components/Home.js
+++ components/Home.js
@@ -25,9 +25,9 @@ const VideoAnalysisPage = () => {
 
     const creators = [
         { name: "Glucose Revolution", image: "/hero/card1.png" },
-        { name: "Esther Perel", image: "/hero/card2.png" },
+        { name: "Jessie Inchauspe", image: "/hero/card2.png" },
         { name: "Ramit Sethi", image: "/hero/card3.png" },
-        { name: "Adam Grant", image: "/hero/card4.png" },
+        { name: "Esther Perel", image: "/hero/card4.png" },
     ];
     const [selectedCreator, setSelectedCreator] = useState(creators[0]);
 
@@ -173,9 +173,9 @@ const HomePage = () => {
 
     const creators = [
         { name: "Glucose Revolution", image: "/hero/card1.png" },
-        { name: "Esther Perel", image: "/hero/card2.png" },
+        { name: "Jessie Inchauspe", image: "/hero/card2.png" },
         { name: "Ramit Sethi", image: "/hero/card3.png" },
-        { name: "Adam Grant", image: "/hero/card4.png" },
+        { name: "Esther Perel", image: "/hero/card4.png" },
     ];
     const [selectedCreator, setSelectedCreator] = useState(creators[0]);
 
