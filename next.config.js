5cf941ae787b3d2321ff5e3ed577893f2d826dfe
SarahJedlaoui <sarajedlaoui999@gmail.com>
2025-02-11 15:39:50 +0100

    updates

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint checks during build
+  },
   reactStrictMode: true,
   swcMinify: false, // Disable SWC minification to prevent loading issues
 }
